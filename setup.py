998257da38e9f7922862a4da37552f0dd1500455
João S. O. Bueno <gwidion@gmail.com>
2013-11-29 02:57:31 -0200

    Adds metadata for pypi publishing

--- setup.py
+++ setup.py
@@ -1,10 +1,51 @@
 # coding: utf-8
+# python-gegl installer
 # Author: João S. O. Bueno
 
+import sys
 from distutils.core import setup
 
-setup(name='gegl',
-      version='0.1',
-      package_dir={'': 'gegl'},
-      packages=[''],
+if "install" in sys.argv:
+    try:
+        from gi.repository import Gegl
+    except ImportError:
+        import sys
+        sys.stderr.write("""\
+            gobject introspection enabled Gegl not installed on this system -
+            this package won't work as is.
+
+            Ensure you have installed:
+                pygobject,
+                a recent gegl build with geobject introspection installed
+
+                Package not installed!
+
+            """)
+        sys.exit(1)
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name = 'python-gegl',
+      version = '0.1',
+      author = 'João S. O. Bueno',
+      author_email = 'gwidion@gmail.com',
+      url = 'https://github.com/jsbueno/python-gegl',
+      description = "Wrappers to simplify GEGL usage with python",
+      package_dir = {'': 'gegl'},
+      packages = [''],
+      long_description = long_description,
+      download_url = 'https://github.com/jsbueno/python-gegl/archive/v0.1.tar.gz',
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Topic :: Multimedia :: Graphics',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Generic Graphics Library',
+          'Topic :: Printing',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        ],
+      license = "LGPL v3"
+
       )
